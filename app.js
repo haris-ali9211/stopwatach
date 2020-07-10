e6b99af3c727a6cafbffc3b9313ce70dfe92939e
haris-ali9211 <haris.aali211@gmail.com>
2020-07-10 20:09:00 +0500

    stopwatch

---
+++ app.js
@@ -0,0 +1,48 @@
+var min = 00;
+var sec = 00;
+var msec = 00;
+
+var minHeading = document.getElementById("min");
+var secHeading = document.getElementById("sec");
+var msecHeading = document.getElementById("msec");
+var interval;
+
+function timer() {
+    msec++
+    msecHeading.innerHTML = msec
+    if (msec >= 100) {
+        sec++
+        secHeading.innerHTML = sec
+        msec = 00
+    } else if (sec >= 60) {
+        min++
+        minHeading.innerHTML = min
+        sec = 00
+    }
+
+}
+
+function start() {
+    interval = setInterval(timer, 10)
+    document.getElementById("disable").disabled = true;
+}
+
+function stop() {
+    clearInterval(interval)
+    document.getElementById("disable").disabled = false;
+
+}
+
+function reset() {
+    min = 00;
+    sec = 00;
+    msec = 00;
+    minHeading.innerHTML = min
+    secHeading.innerHTML = sec
+    msecHeading.innerHTML = msec
+    stop()
+}
+
+function lap() {
+    alert(min+":"+sec+":"+msec)
+}
